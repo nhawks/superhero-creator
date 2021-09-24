c22f9f2abbd09382369d85e3df3fe595e3bc6962
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-24 11:33:43 -0400

    initial creaton of edit function

--- superhero_creator/superheroes/views.py
+++ superhero_creator/superheroes/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
 from django.http import HttpResponseRedirect
-
 from django.urls import reverse
-
 from .models import Superhero
 
 
@@ -36,16 +32,51 @@ def create(request):
 
         #?Instantiate Superhero Objects
         new_hero = Superhero(
-                            name = name, 
-                            alter_ego = alter_ego,
-                            primary_ability = primary_ability, 
-                            secondary_ability = secondary_ability, 
-                            catch_phrase = catch_phrase,
-                            )
+            name = name, 
+            alter_ego = alter_ego,
+            primary_ability = primary_ability, 
+            secondary_ability = secondary_ability, 
+            catch_phrase = catch_phrase,
+        )
 
         #?save to Superhero DB
         new_hero.save()
         #?return user to superheroes/index.html
         return HttpResponseRedirect(reverse('superheroes:index'))
+
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    edit_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'edit_hero': edit_hero
+    }
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary')
+        secondary_ability = request.POST.get('secondary')
+        catch_phrase = request.POST.get('catch_phrase')
+
+        edit_hero = Superhero(
+            name = name, 
+            alter_ego = alter_ego,
+            primary_ability = primary_ability, 
+            secondary_ability = secondary_ability, 
+            catch_phrase = catch_phrase,
+        )
+
+        #?save to Superhero DB
+        edit_hero.save()
+        #?return user to superheroes/index.html
+        return HttpResponseRedirect(reverse('superheroes:index'))
+        
+    else:
+        return render(request, 'superheroes/edit.html')
+
+
+    
+        
+
