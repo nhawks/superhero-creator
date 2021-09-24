49d5a534d2a5f4900b71a1ac6409d19179072b67
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-23 23:16:09 -0400

    created 'create' path to allow users to create new heroes

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('details/<int:hero_id>', views.details, name ='details')
+    path('details/<int:hero_id>', views.details, name ='details'),
+    path('new/', views.create, name='create'),
 
 ]
--- superhero_creator/superheroes/views.py
+++ superhero_creator/superheroes/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
 from .models import Superhero
 
 
@@ -19,3 +23,29 @@ def details(request, hero_id):
         'single_hero': single_hero
     }
     return render(request, 'superheroes/details.html', context)
+
+def create(request):
+    if request.method == "POST":
+        #todo - save the form content as new db object
+        #todo - return to index
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary')
+        secondary_ability = request.POST.get('secondary')
+        catch_phrase = request.POST.get('catch_phrase')
+
+        #?Instantiate Superhero Objects
+        new_hero = Superhero(
+                            name = name, 
+                            alter_ego = alter_ego,
+                            primary_ability = primary_ability, 
+                            secondary_ability = secondary_ability, 
+                            catch_phrase = catch_phrase,
+                            )
+
+        #?save to Superhero DB
+        new_hero.save()
+        #?return user to superheroes/index.html
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
