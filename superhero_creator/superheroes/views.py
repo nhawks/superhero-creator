e2bc4665d92afae477c889f1c1c7eb3d59b47dd1
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-24 12:24:00 -0400

    corrected views functon to update hero

--- superhero_creator/superheroes/views.py
+++ superhero_creator/superheroes/views.py
@@ -22,8 +22,6 @@ def details(request, hero_id):
 
 def create(request):
     if request.method == "POST":
-        #todo - save the form content as new db object
-        #todo - return to index
         name = request.POST.get('name')
         alter_ego = request.POST.get('alter_ego')
         primary_ability = request.POST.get('primary')
@@ -54,29 +52,16 @@ def edit(request, hero_id):
     }
 
     if request.method == "POST":
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary_ability = request.POST.get('primary')
-        secondary_ability = request.POST.get('secondary')
-        catch_phrase = request.POST.get('catch_phrase')
-
-        edit_hero = Superhero(
-            name = name, 
-            alter_ego = alter_ego,
-            primary_ability = primary_ability, 
-            secondary_ability = secondary_ability, 
-            catch_phrase = catch_phrase,
-        )
-
+        edit_hero.name = request.POST.get('name')
+        edit_hero.alter_ego = request.POST.get('alter_ego')
+        edit_hero.primary_ability = request.POST.get('primary')
+        edit_hero.secondary_ability = request.POST.get('secondary')
+        edit_hero.catch_phrase = request.POST.get('catch_phrase')
+        
         #?save to Superhero DB
         edit_hero.save()
         #?return user to superheroes/index.html
         return HttpResponseRedirect(reverse('superheroes:index'))
         
     else:
-        return render(request, 'superheroes/edit.html')
-
-
-    
-        
-
+        return render(request, 'superheroes/edit.html', context)
