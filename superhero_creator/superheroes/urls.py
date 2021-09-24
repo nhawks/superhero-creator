1c2e3c15a18c9858bad234fc3448d07fe3166c4b
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-24 11:35:51 -0400

    added edit path to app url

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('details/<int:hero_id>', views.details, name ='details'),
-    path('new/', views.create, name='create'),
-
+    path('details/<int:hero_id>', views.details, name = 'details'),
+    path('new/', views.create, name ='create'),
+    path('edit/', views.edit, name = 'edit'),
 ]
