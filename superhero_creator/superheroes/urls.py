56eae7190a302d02a2afbb1025682d92ec1a1293
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-24 13:52:30 -0400

    removed string quotation marks from views.delete path

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('details/<int:hero_id>', views.details, name = 'details'),
     path('new/', views.create, name ='create'),
     path('edit/<int:hero_id>', views.edit, name = 'edit'),
-    path('delete/<int:hero_id>', 'views.delete', name = 'delete'),
+    path('delete/<int:hero_id>', views.delete , name = 'delete'),
 ]
